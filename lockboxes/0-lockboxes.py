7ea27d39176db6abfed14dc49cca73affc3b29bc
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-15 17:11:10 +0000

    Task 0 rebuild without recursion

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -2,24 +2,15 @@
 """Lockboxes Module 0"""
 
 
-def canUnlock(box, boxes, unlocked):
-    """Check keys in boxes"""
-    for keys in box:
-        if keys in unlocked or keys > len(boxes) - 1:
-            continue
-        unlocked.append(keys)
-        if keys > boxes.index(box):
-            continue
-        canUnlock(boxes[keys], boxes, unlocked)
-
-
 def canUnlockAll(boxes):
     """Can unlock all boxes?"""
     unlocked = [0]
-    if len(boxes) == 0:
-        return True
-    for i in range(len(boxes)):
-        if i not in unlocked:
+    for box_idx, box in enumerate(boxes):
+        if not box:
             continue
-        canUnlock(boxes[i], boxes, unlocked)
-    return False if len(unlocked) != len(boxes) else True
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_idx:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
