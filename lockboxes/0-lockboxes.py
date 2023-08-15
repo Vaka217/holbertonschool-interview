03d2b9938ce08b96171c00fdbe13d1664321d549
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-15 15:39:12 +0000

    Task 0

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Lockboxes Module 0"""
+
+def canUnlock(box, boxes, unlocked):
+    """Check keys in boxes"""
+    for key in box:
+        if key in unlocked or key > len(boxes):
+            continue
+        unlocked.append(key)
+        canUnlock(boxes[key], boxes, unlocked)
+        
+        
+
+def canUnlockAll(boxes):
+    """Can unlock all boxes?"""
+    unlocked = [0]
+    if len(boxes) == 0:
+        return True
+    for i in range(len(boxes)):
+        if i not in unlocked:
+            continue
+        canUnlock(boxes[i], boxes, unlocked) 
+    return False if len(unlocked) != len(boxes) else True
