c0378fddb75830a1bf3a567ffc303f57b3f7d356
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-08-31 18:48:03 -0700

    Add leaves function to make the tree and change the while for the max heap as another function

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,36 +1,52 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "binary_trees.h"
 
-/*
- * heaping_time - Iterates back through parent to acommodate the values
- *
- * @node: Pointer to the heaping node
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * Return: The number of leaves, 0 otherwise
+ * @tree: pointer to the root node of the tree to count the number of leaves
  */
-void heaping_time(heap_t *node)
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	while (node->parent && node->n > node->parent->n)
+	size_t leaves = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		leaves++;
+	if (tree->left)
+		leaves += binary_tree_leaves(tree->left);
+	if (tree->right)
+		leaves += binary_tree_leaves(tree->right);
+	return (leaves);
+}
+
+/**
+ * heaping_time - Restores the Max Heap property by moving a node value up
+ * @new: A pointer to the node to be move
+ * @value: Value to be passed
+ */
+void heaping_time(heap_t *new, int value)
+{
+	while (new->parent && (new->n > new->parent->n))
 	{
-		int tmp = node->n;
-
-		node->n = node->parent->n;
-		node->parent->n = tmp;
-
-		node = node->parent;
+		new->n = new->parent->n;
+		new->parent->n = value;
+		new = new->parent;
 	}
 }
 
 /**
  * heap_insert - Inserts a value into a Max Binary Heap
- *
  * @root: Double pointer to the root node of the Heap
  * @value: Value to put in the new node
- *
  * Return: Pointer to the inserted node, or NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new, *current;
+	heap_t *new, *parent;
+	size_t leaves = 0;
 
 	if (!root)
 		return (NULL);
@@ -38,36 +54,29 @@ heap_t *heap_insert(heap_t **root, int value)
 	new = binary_tree_node(NULL, value);
 	if (!new)
 		return (NULL);
+
 	if (!*root)
 	{
 		*root = new;
 		return (new);
 	}
-	current = *root;
-	while (current)
+
+	leaves = binary_tree_leaves(*root);
+	parent = *root;
+	while (parent->left && parent->right)
 	{
-		if (!current->left)
-		{
-			current->left = new;
-			new->parent = current;
-			break;
-		}
-		else if (!current->right)
-		{
-			current->right = new;
-			new->parent = current;
-			break;
-		}
-		if (current->left->left && current->left->right && !current->right->right)
-			current = current->right;
+		if (leaves > 0 && leaves % 2 == 0)
+			parent = parent->left;
 		else
-			current = current->left;
+			parent = parent->right;
 	}
-	while (new->parent && (new->n > new->parent->n))
-	{
-		new->n = new->parent->n;
-		new->parent->n = value;
-		new = new->parent;
-	}
+
+	if (!parent->left)
+		parent->left = new;
+	else
+		parent->right = new;
+
+	new->parent = parent;
+	heaping_time(new, value);
 	return (new);
 }
