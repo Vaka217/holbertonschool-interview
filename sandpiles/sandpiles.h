e2d0a16ff439a43d44e12f05210d59b8558218f5
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-09-12 15:13:34 +0000

    Task 0

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,9 @@
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
+
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void sandpile_topple(int grid[3][3]);
+
+#endif /* _SANDPILES_H_ */
