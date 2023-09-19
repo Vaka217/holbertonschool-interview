b069e222ea654220487ac5140931371c800378c0
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-09-19 15:21:28 -0300

    Palindrome task

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	const listint_t *current = *head;
+	unsigned int i, n = 0;
+
+	if (!*head || !(*head)->next)
+		return (1);
+
+	while (current)
+	{
+		current = current->next;
+		n++;
+	}
+
+	int half_list[n / 2];
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		half_list[i] = (*head)->n;
+		*head = (*head)->next;
+	}
+
+	if (n % 2 != 0)
+		*head = (*head)->next;
+
+	for (i = (n / 2); i > 0; i--)
+	{
+		if ((*head)->n != half_list[i - 1])
+			return (0);
+		*head = (*head)->next;
+	}
+	return (1);
+}
