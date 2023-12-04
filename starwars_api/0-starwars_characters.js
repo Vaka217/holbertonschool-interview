0e3e516d86f0e9b4937e279e88bb84a9ff254e62
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-12-04 11:29:27 -0300

    Task 0

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const request = require("request");
+
+const getCharacterName = (character) => {
+  return new Promise((resolve, reject) => {
+    request(character, (err, response, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+};
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  async (err, response, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const characters = JSON.parse(body).characters;
+
+      for (const character of characters) {
+        const name = await getCharacterName(character);
+        console.log(name);
+      }
+    }
+  }
+);
