9171e42d2cfd4c54bd4fd7bdbb910e4144f4047b
Gonzalo Cabaleiro <98929961+Vaka217@users.noreply.github.com>
2024-01-05 22:06:55 -0300

    Create separate function for searching the index of the value

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -1,52 +1,57 @@
 #include "search_algos.h"
 
 /**
+ * find_value - searches for the first index with the value in the array
+ * @array: Sorted array
+ * @left: Index of the start of the subarray
+ * @right: Index of the end of the subarray
+ * @value: Value to search for
+ * Return: The index where the value is located, -1 otherwise
+ */
+
+int find_value(int *array, size_t left, size_t right, int value)
+{
+	size_t i, idx, middle, len;
+
+	if (left == right)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (i = left; i < right - 1; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i + 1]);
+
+	middle = idx = (left + right - 1) / 2;
+	len = left - right;
+
+	if (len > 0 && (array[idx] == value) && (array[idx - 1] > value))
+		middle = idx;
+	else if ((array[idx] == value) && (array[idx - 1] == value))
+		middle = find_value(array, left, idx + 1, value);
+	else if (an_array[idx] < value)
+		middle = find_value(array, idx + 1, right, value);
+	else if (an_array[idx] > value)
+		middle = find_value(array, left, idx + 1, value);
+	else if (an_array[idx] == value)
+		middle = idx;
+	else
+		middle = -1;
+
+	return (middle);
+}
+
+/**
  * advanced_binary - searches for a value in a sorted array of integers
  * @array: Pointer to the first element of the array to search in
  * @size: The number of elements in array
  * @value: Value to search for
  * Return: The index where the value is located, -1 otherwise
  */
+
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t i;
-	int m, result, first = -1;
+	int idx;
 
-	if (!array)
-		return (-1);
-
-	m = size / 2;
-
-	if (size == 1 && array[m] == value)
-		return (m);
-
-	printf("Searching in array: ");
-	for (i = 0; i < size - 1; i++)
-	{
-		if (array[i] == value && array[first] != value)
-			first = i;
-		printf("%d, ", array[i]);
-	}
-	printf("%d\n", array[size - 1]);
-
-	if (size == 2 && array[m] == value)
-	{
-		if (array[m - 1] && array[m - 1] == value)
-			return (m - 1);
-		return (m);
-	}
-
-	if (size == 1)
-		return (-1);
-
-	if (size % 2 != 0 && array[m] == value)
-		return (m);
-	else if (array[m] > value || (array[m] >= value && m != first))
-		return (advanced_binary(array, m, value));
-
-	if (size % 2 == 0)
-		m--;
-
-	result = advanced_binary(array + m + 1, size - m - 1, value);
-	return ((result == -1) ? -1 : m + 1 + result);
+	idx = (!array || size < 1) ? -1 : find_value(array, 0, size, value);
+	return (idx);
 }
