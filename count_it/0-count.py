8e70b814384a92beebb6976c5ddf2e57e099af39
Vaka217 <gonzalocabaleiro12@gmail.com>
2024-01-09 16:56:24 -0300

    Task 0 now with recursive function

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -1,33 +1,67 @@
 #!/usr/bin/env python3
 """Count It Module"""
 import requests
-import time
+
+after = None
+
+
+def recurrent_search(subreddit, hot_articles=[]):
+    """Recursive function that queries the Reddit API and parses the title of
+    all hot articles"""
+    try:
+        global after
+        url = "https://www.reddit.com/r/"
+
+        if after:
+            response = requests.get(url + subreddit + "/hot.json?after=" +
+                                    after, headers={
+                                        'User-agent': 'yourbot'},
+                                    allow_redirects=False)
+        else:
+            response = requests.get(url + subreddit + "/hot.json",
+                                    headers={'User-agent': 'yourbot'},
+                                    allow_redirects=False)
+
+        after = response.json()["data"]["after"]
+        hot_articles += [article["data"]["title"]
+                         for article in response.json()["data"]["children"]]
+
+        if after:
+            recurrent_search(subreddit, hot_articles)
+
+        return hot_articles
+    except Exception:
+        return None
 
 
 def count_words(subreddit, word_list):
-    """Queries the Reddit API, parses the title of all hot articles, and
-    prints a sorted count of given keywords (case-insensitive, delimited
-    by spaces.)"""
+    """Prints a sorted count of given keywords (case-insensitive, delimited
+    by spaces).
+    Results should be printed in descending order, by the count, and if the
+    count is the same for separate keywords, they should then be sorted
+    alphabetically (ascending, from A to Z).
+    Words with no matches should be skipped and not printed. Words must be
+    printed in lowercase.
+    Results are based on the number of times a keyword appears, not titles it
+    appears in. java java java counts as 3 separate occurrences of java.
+    To make life easier, java. or java! or java_ should not count as java
+    If no posts match or the subreddit is invalid, print nothing."""
+    hot_articles = recurrent_search(subreddit)
 
-    response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
+    if hot_articles is None:
+        return None
 
-    if response.status_code != 200:
-        return
+    count = {word: 0 for word in word_list}
+    for article in hot_articles:
+        for word in word_list:
+            if word.lower() in article:
+                count[word] += 1
 
-    base_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    request = requests.get(base_url, headers={'User-agent': 'yourbot'})
+    count_sorted = sorted(count.items(),
+                          key=lambda x: x[0])
+    count_sorted = sorted(count_sorted,
+                          key=lambda x: x[1], reverse=True)
 
-    if request.status_code != 200:
-        print(f"Error: {request.status_code}")
-        return
-
-    posts = []
-    for post in request.json()['data']['children']:
-        x = post['data']['title']
-        posts.append(x)
-    posts = ' '.join(posts).lower()
-    print(posts)
-    for word in word_list:
-        matchings = posts.count(word.lower())
-        if matchings > 0:
-            print(f"{word}:", posts.count(word.lower()))
+    for word in count_sorted:
+        if word[1] > 0:
+            print(f"{word[0]}: {word[1]}")
