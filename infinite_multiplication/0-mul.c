9e3a3ddab836cdb6dfa1ecd560856de167a0ce35
Vaka217 <gonzalocabaleiro12@gmail.com>
2024-01-28 14:13:19 -0300

    Infinite multiplication task now with separate functions and descriptions for each one

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -1,5 +1,9 @@
-#include "holberton.h"
-
+/**
+ * str_len - Computes the length of a string.
+ * @str: Pointer to the string to be measured.
+ *
+ * Return: Length of the string.
+ */
 int str_len(char *str)
 {
 	int i = 0;
@@ -10,6 +14,9 @@ int str_len(char *str)
 	return (i);
 }
 
+/**
+ * print_error - Prints "Error" followed by a newline character.
+ */
 void print_error(void)
 {
 	_putchar('E');
@@ -20,6 +27,12 @@ void print_error(void)
 	_putchar('\n');
 }
 
+/**
+ * is_digit - Checks if a string consists only of numeric digits.
+ * @str: Pointer to the string to be checked.
+ *
+ * Return: 1 if string contains only digits, 0 otherwise.
+ */
 int is_digit(char *str)
 {
 	int i = 0;
@@ -33,6 +46,13 @@ int is_digit(char *str)
 	return (1);
 }
 
+/**
+ * main - Entry point of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of pointers to the arguments.
+ *
+ * Return: Always 0 (Success).
+ */
 int main(int argc, char *argv[])
 {
 	char *s_a, *s_b;
@@ -46,44 +66,32 @@ int main(int argc, char *argv[])
 		print_error();
 		exit(98);
 	}
-
 	s_a = argv[1], s_b = argv[2];
 	size_a = str_len(s_a), size_b = str_len(s_b);
 	a = malloc(sizeof(int) * size_a), b = malloc(sizeof(int) * size_b);
 	result = malloc(sizeof(int) * (size_a + size_b));
-
 	for (a_i = size_a - 1, sa_i = 0; a_i >= 0; a_i--, sa_i++)
 		a[a_i] = s_a[sa_i] - '0';
-
 	for (b_i = size_b - 1, sb_i = 0; b_i >= 0; b_i--, sb_i++)
 		b[b_i] = s_b[sb_i] - '0';
-
 	for (b_i = 0; b_i < size_b; b_i++)
 	{
 		for (a_i = 0; a_i < size_a; a_i++)
-		{
 			result[a_i + b_i] += a[a_i] * b[b_i];
-		}
 	}
-
 	for (r_i = 0; r_i < (size_a + size_b); r_i++)
 	{
 		carry = result[r_i] / 10;
 		result[r_i] = result[r_i] % 10;
 		result[r_i + 1] += carry;
 	}
-
 	for (r_i = (size_a + size_b - 1); r_i >= 0; r_i--)
 	{
 		if (result[r_i] != 0)
 			break;
 	}
-
 	for (; r_i >= 0; r_i--)
-	{
 		_putchar(result[r_i] + '0');
-	}
 	_putchar('\n');
-
 	return (0);
 }
---
+++ infinite_multiplication/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdlib.h>
+
+int is_digit(char *str);
+void print_error(void);
+int str_len(char *str);
+int _putchar(char c);
+
+#endif
