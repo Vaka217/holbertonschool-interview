6e7bd85cff00a48e861f51a4244cddc4ddd0c834
Vaka217 <gonzalocabaleiro12@gmail.com>
2024-03-29 17:58:18 -0300

    AVL Trees task

---
+++ avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,77 @@
+#include "binary_trees.h"
+
+/**
+ * calculate_height - calculates the height of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: height of the tree
+ **/
+int calculate_height(binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (!tree)
+		return (0);
+
+	left_height = calculate_height(tree->left);
+	right_height = calculate_height(tree->right);
+
+	return (left_height > right_height ? left_height + 1 : right_height + 1);
+}
+
+/**
+ * calculate_balance - calculates the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: balance factor of the tree
+ **/
+int calculate_balance(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (!tree)
+		return (0);
+
+	left_height = calculate_height(tree->left);
+	right_height = calculate_height(tree->right);
+
+	return (left_height - right_height);
+}
+
+/**
+ * is_bst_helper - helper function to check if a binary tree
+ * is a binary search tree
+ * @tree: pointer to the root node of the tree
+ * @min: minimum value allowed in the tree
+ * @max: maximum value allowed in the tree
+ * Return: 1 if the tree is a BST, otherwise 0
+ **/
+int is_bst_helper(const binary_tree_t *tree, int min, int max)
+{
+	if (!tree)
+		return (1);
+
+	if (tree->n < min || tree->n > max)
+		return (0);
+
+	return (is_bst_helper(tree->left, min, tree->n - 1) &&
+		  is_bst_helper(tree->right, tree->n + 1, max));
+}
+
+/**
+ * binary_tree_is_avl - checks if a binary tree is an AVL tree and a binary
+ * search tree
+ * @tree: pointer to the root node of the tree
+ * Return: 1 if the tree is an AVL tree, otherwise 0
+ **/
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int balance;
+
+	if (!tree)
+		return (0);
+
+	if (!is_bst_helper(tree, INT_MIN, INT_MAX))
+		return (0);
+
+	balance = calculate_balance(tree);
+	return (balance >= -1 && balance <= 1);
+}
---
+++ avl_trees/binary_trees.h
@@ -0,0 +1,33 @@
+#ifndef BINARY_TREES
+#define BINARY_TREES
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+#define MY_INT_MIN INT_MIN
+
+#define MY_INT_MAX INT_MAX
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+void binary_tree_print(const binary_tree_t *);
+int binary_tree_is_avl(const binary_tree_t *tree);
+int is_bst_helper(const binary_tree_t *tree, int min, int max);
+int calculate_balance(const binary_tree_t *tree);
+int calculate_height(binary_tree_t *tree);
+#endif
